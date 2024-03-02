fc5f9365e031191090729a7bfe0bedc9e65da1e9
AdrianTheFrog <afsw759@gmail.com>
2024-03-02 16:21:34 -0500

    smooth camera follow

---
+++ Camera2D.gd
@@ -0,0 +1,11 @@
+extends Camera2D
+
+var lookAheadPos = Vector2.ZERO
+
+func _process(delta):
+	var speed = 0.1
+	var lookAheadDistance = 0.15
+	var lookAheadResponse = 0.05
+	
+	lookAheadPos = lookAheadPos*(1-lookAheadResponse) + lookAheadDistance*%Character.get_real_velocity ()*lookAheadResponse
+	set_global_position(get_global_position()*(1-speed)+(%Character.get_global_position()+lookAheadPos)*speed)
