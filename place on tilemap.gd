100813ed2654f1a210dabd8c59e67234ea84117d
AdrianTheFrog <afsw759@gmail.com>
2024-03-02 20:32:12 -0500

    Make placable buildings, smooth power plant collision

---
+++ place on tilemap.gd
@@ -0,0 +1,7 @@
+extends TileMap
+
+func _input(event):
+	# Mouse in viewport coordinates.
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed() == true:
+		set_cell(0, Vector2i(floor(get_local_mouse_position()/32)), 1, Vector2i(0,0))
+		print(ceil(get_local_mouse_position()/32))
