7f33ecf09309b9c76d255f79c9f767b4450027cb
AdrianTheFrog <afsw759@gmail.com>
2024-03-02 21:14:56 -0500

    procedural generation and use delta variable more

--- Camera2D.gd
+++ Camera2D.gd
@@ -3,10 +3,10 @@ extends Camera2D
 var lookAheadPos = Vector2.ZERO
 
 func _process(delta):
-	var speed = 0.025
-	var lookAheadDistance = 0.2
-	var lookAheadResponse = 0.01
+	var speed = 2.5
+	var lookAheadDistance = .5
+	var lookAheadResponse = 1.0
 	
-	lookAheadPos = lookAheadPos*(1-lookAheadResponse) + lookAheadDistance*%Character.get_real_velocity ()*lookAheadResponse
-	set_global_position(get_global_position()*(1-speed)+(%Character.get_global_position()+lookAheadPos)*speed)
+	lookAheadPos = lookAheadPos*(1-(lookAheadResponse)*delta) + lookAheadDistance*%Character.get_real_velocity()*lookAheadResponse*delta
+	set_global_position(get_global_position()*(1-(speed*delta))+(%Character.get_global_position()+lookAheadPos)*speed*delta)
 	zoom = Vector2(get_viewport().size.x/500.,get_viewport().size.x/500.).ceil()
--- TileMap.gd
+++ TileMap.gd
@@ -1,11 +1,25 @@
 extends TileMap
+var moisture = FastNoiseLite.new()
+var temperature = FastNoiseLite.new()
+var altitude = FastNoiseLite.new()
+var width = 500/16
+var height = 500/16
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	moisture.seed = randi()
+	temperature.seed = randi()
+	altitude.seed = randi()
 
+func _process(_delta):
+	generate_chunk(%Character.position)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func generate_chunk(pos):
+	var tile_pos = local_to_map(pos)
+	for x in range(width):
+		for y in range(height):
+			var moist = moisture.get_noise_2d(tile_pos.x-width/2 + x,tile_pos.y-height/2 + y)*10
+			var temp = temperature.get_noise_2d(tile_pos.x-width/2 + x,tile_pos.y-height/2 + y)*10
+			var alt = altitude.get_noise_2d(tile_pos.x-width/2 + x,tile_pos.y-height/2 + y)*10
+			set_cell(0,Vector2i(tile_pos.x-width/2 + x,tile_pos.y-height/2 + y),1, Vector2(round((moist+10)/5), round((temp+10)/5)))
+
+ 
