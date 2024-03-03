7ec0a4819c9727ef4cb1f703a5da318f9d046230
William <william.sarnowski127@gmail.com>
2024-03-03 11:58:27 -0500

    added cave and bank, added descriptions for each building
    still a little scetch

--- main scene.gd
+++ main scene.gd
@@ -7,10 +7,6 @@ var oil = 0
 var bits = 0
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	
-
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,17 +19,96 @@ func _process(delta):
 	$Camera2D/bitsui.text = ":" + str(bits)
 	
 	
-		
+	
+	
 	pass
 
 
 func _on_timer_timeout():
 	$Camera2D/Climatechange.value += 1 #put multiplicaton from buildings here
+	pass # Replace with function body.
 
 
+func _on_powerplantbutton_pressed():
+	var powerlabel = get_node("Camera2D/powerplanttext")
+	var powerback = get_node("Camera2D/powerplantpanel")
+	powerback.visible = true
+	powerlabel.visible = true
+	pass # Replace with function body.
 
 
+func _on_waterpumpbutton_pressed():
+	var waterlabel = get_node("Camera2D/waterplanttext")
+	var waterback = get_node("Camera2D/waterplantpanel")
+	waterback.visible = true
+	waterlabel.visible = true
+	pass # Replace with function body.
 
 
+func _on_oilpumpbutton_pressed():
+	var oillabel = get_node("Camera2D/oiltext")
+	var oilback = get_node("Camera2D/oilpumppanel")
+	oilback.visible = true
+	oillabel.visible = true
 	
 	pass # Replace with function body.
+
+
+func _on_recyclebutton_pressed():
+	var recyclelabel = get_node("Camera2D/recycletext")
+	var recycleback = get_node("Camera2D/recyclepanel")
+	recycleback.visible = true
+	recyclelabel.visible = true
+	pass # Replace with function body.
+
+func _on_bankbutton_pressed():
+	var banklabel = get_node("Camera2D/banktext")
+	var bankback = get_node("Camera2D/bankpanel")
+	bankback.visible = true
+	banklabel.visible = true
+	pass # Replace with function body.
+
+func _on_coalminebutton_pressed():
+	var coallabel = get_node("Camera2D/coalminetext")
+	var coalback = get_node("Camera2D/coalminepanel")
+	coalback.visible = true
+	coallabel.visible = true
+	pass # Replace with function body.
+
+func _on_timer_for_ui_timeout():
+	var oillabel = get_node("Camera2D/oiltext")
+	var oilback = get_node("Camera2D/oilpumppanel")
+	oilback.visible = false
+	oillabel.visible = false
+	
+	var powerlabel = get_node("Camera2D/powerplanttext")
+	var powerback = get_node("Camera2D/powerplantpanel")
+	powerback.visible = false
+	powerlabel.visible = false
+	
+	var waterlabel = get_node("Camera2D/waterplanttext")
+	var waterback = get_node("Camera2D/waterplantpanel")
+	waterback.visible = false
+	waterlabel.visible = false
+	
+	var recyclelabel = get_node("Camera2D/recycletext")
+	var recycleback = get_node("Camera2D/recyclepanel")
+	recycleback.visible = false
+	recyclelabel.visible = false
+	
+	var banklabel = get_node("Camera2D/banktext")
+	var bankback = get_node("Camera2D/bankpanel")
+	bankback.visible = false
+	banklabel.visible = false
+	
+	var coallabel = get_node("Camera2D/coalminetext")
+	var coalback = get_node("Camera2D/coalminepanel")
+	coalback.visible = false
+	coallabel.visible = false
+	pass # Replace with function body.
+
+
+
+
+
+
